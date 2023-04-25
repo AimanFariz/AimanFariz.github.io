b187da74e388030ffccf37ce2a12172e3b3b8985
AimanFariz <aimanfzpermatainsan@gmail.com>
2023-04-26 00:12:11 +0800

    flex-wrapped da blogs

--- truncate.js
+++ truncate.js
@@ -1,39 +1,18 @@
-// const pTag = document.getElementById('description'); // Get the <p> element by ID
-// const maxLength = 10; // Maximum number of characters allowed
-// const text = pTag.textContent; // Get the text content of the <p> element
+let pTag = document.getElementById('pTag')
+let text = pTag.innerHTML
 
-// if (text.length > maxLength) {
-//   // If the text length exceeds the maximum length
-//   const truncatedText = text.slice(0, maxLength) + '...'; // Truncate the text and add '...' at the end
-//   pTag.textContent = truncatedText; // Update the text content of the <p> element
-// }
+function truncateString(str, num) {
+  let s = str.slice()
+  let result = ''
+  for(let i = 0; i < s.length; i++){
+    if(result.length < num){
+      result += s[i]
+    }else if(result.length === num){
+      result += '...'
+    }
+  }
+  pTag.innerHTML = result; // Set the truncated text as innerHTML of 'final' element
+}
 
-const text = document.getElementById('description').innerHTML; // Get the <p> element by ID
-const maxLength = 10; // Maximum number of characters allowed
-let result = ''
-
-
-for(let i = 0;i<text.length;i++){
-        if(result.length < maxLength){
-          result+=text[i]
-        }else if(text.length == num){
-          result += '...'
-        }
-        document.getElementById('description').innerHTML = result
-      }
-
-// function truncateString(str, num) {
-//     let s = str.slice()
-//     let result = ''
-//     for(let i = 0;i<s.length;i++){
-//       if(result.length < num){
-//         result+=str[i]
-//       }else if(result.length == num){
-//         result += '...'
-//       }
-//     }
-//     return result;
-//   }
-
-// document.getElementById("truncate") = truncateString(desc, 20)
+truncateString(text, 3); // Call the function with desired number of characters to truncate
 
